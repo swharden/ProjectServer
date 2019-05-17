0d55c6fe76bcab2164b814d6f92f10d00c49af0c
Scott W Harden <swharden@gmail.com>
2019-05-17 11:00:50 -0400

    setpath loads ABF into OriginLab using COM

--- src/AbfBrowser/Displays/DisplayLabtalk.cs
+++ src/AbfBrowser/Displays/DisplayLabtalk.cs
@@ -15,10 +15,28 @@ namespace AbfBrowser
             Debug.WriteLine($"Creating labtalk display from {response}");
         }
 
+        public void ExecuteLabTalk(string ltCommand)
+        {
+            Debug.WriteLine($"connecting to Origin instance...");
+            Origin.ApplicationSI origin = new Origin.ApplicationSIClass();
+
+            Debug.WriteLine($"focusing and maximizing OriginLab...");
+            origin.Visible = Origin.MAINWND_VISIBLE.MAINWND_SHOW_BRING_TO_FRONT;
+            origin.Visible = Origin.MAINWND_VISIBLE.MAINWND_SHOWMAXIMIZED;
+
+            Debug.WriteLine($"Executing labtalk command [{ltCommand}]...");
+            origin.Execute(ltCommand);
+
+            Debug.WriteLine($"disconnecting from Origin...");
+            System.Runtime.InteropServices.Marshal.FinalReleaseComObject(origin);
+            Debug.WriteLine($"Origin instance released.");
+        }
+
         public override string GetHTML()
         {
             string ltCommand = Html.UrlDecode(response.request.value);
             string html = $"Executing LabTalk:<br><code>{ltCommand}</code>";
+            ExecuteLabTalk(ltCommand);
             return Html.BuildPage(html, response.GetStatus());
         }
 
