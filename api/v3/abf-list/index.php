e0259c3fec1fb1d2bf64689d343660ee87d79e0f
Scott W Harden <swharden@gmail.com>
2022-03-25 17:08:03 -0400

    API: refactor abf-list

--- api/v3/abf-list/index.php
+++ api/v3/abf-list/index.php
@@ -1,28 +1,22 @@
 <?php
+header("Access-Control-Allow-Origin: *");
+
 require_once("../../shared.php");
 require_once("../../phpABF/ABF.php");
 
-$postString = file_get_contents('php://input');
-if (strpos($postString, '\\'))
-    errorAndDie(400, "JSON Error", "do not use backslashes (only forward slashes)");
+if (!isset($_GET["folder"]))
+    errorAndDie(400, "URL Error", "'folder' is required");
 
-$post = json_decode($postString, true);
-
-if (json_last_error() != JSON_ERROR_NONE)
-    errorAndDie(400, "JSON Error", "json_last_error() returned " . json_last_error());
-
-if (!array_key_exists("abf-folder-path", $post))
-    errorAndDie(400, "JSON Error", "abf-folder-path is required");
-
-$abfFolderPath = $post["abf-folder-path"];
+$abfFolderPath = str_replace('\\', '/', $_GET["folder"]);
 $abfFolderPath = LocalPathFromX($abfFolderPath);
 if (!is_dir($abfFolderPath))
-    errorAndDie(500, "Server Error", "folder does not exist");
+    errorAndDie(500, "Server Error", "folder does not exist: $abfFolderPath");
 
-$fileNames = scandir($abfFolderPath);
+$fileNames = array_slice(scandir($abfFolderPath), 2);
 
 $obj = (object)[
     "abf-folder-path" => LocalPathToX(realpath($abfFolderPath)),
+    "filenames" => $fileNames,
     "abfs-by-parent" => GetAbfsByParent($fileNames),
 ];
 
