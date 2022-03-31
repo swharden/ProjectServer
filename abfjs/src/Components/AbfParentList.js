0f516454eea3c594ee52c0d3d0bfc5546162aded
Scott W Harden <swharden@gmail.com>
2022-03-31 17:58:10 -0400

    component: menu items use new API

--- abfjs/src/Components/AbfParentList.js
+++ abfjs/src/Components/AbfParentList.js
@@ -1,37 +1,59 @@
 import React from 'react';
-import AbfParent from '../Models/AbfParent';
+
+class AbfListItem {
+    constructor(abfID, childCount, comment, color) {
+        this.abfID = abfID;
+        this.childCount = childCount;
+        this.comment = comment;
+        this.color = color;
+    }
+
+    getHtml() {
+        return (
+            <div key={this.abfID} style={{ fontFamily: 'monospace', fontSize: '.8em', }}>
+
+                <span style={{ backgroundColor: this.color }}>
+                    {this.abfID}
+                </span>
+
+                <div style={{ display: 'inline-block' }}>
+                    &nbsp;({this.childCount})&nbsp;
+                </div>
+
+                <div style={{ display: 'inline-block' }}>
+                    {this.comment}
+                </div>
+
+            </div>
+        );
+    }
+}
 
 class AbfParentList extends React.Component {
+
+    componentDidMount() {
+        this.fetchParents();
+    }
 
     state = {
         parents: null,
         folder: "folder not set",
-        parents: this.getInitialParents(),
     };
 
-    getInitialParents() {
-        let parents = [];
-        parents.push(new AbfParent("x:/folder/path1.abf"));
-        parents.push(new AbfParent("x:/folder/path2.abf"));
-        parents.push(new AbfParent("x:/folder/path3.abf"));
-        return parents;
-    }
-
-    parentsFromJson(json) {
-
-        let parents = [];
-        json.filenames.forEach(filename => {
-            const abfPath = json.folder + "/" + filename;
-            const parent = new AbfParent(abfPath);
-            parents.push(parent);
+    parentsFromJson = (json) =>
+        Object.keys(json).map(parentID => {
+            return new AbfListItem(
+                parentID,
+                json[parentID]["childCount"],
+                json[parentID]["comment"],
+                json[parentID]["color"]
+            );
         });
 
-        return parents;
-    }
-
     fetchParents() {
-        const folderPath = `X:/Data/SD/practice/Scott/2022/2022-01-04-AON`
-        const url = `http://192.168.1.9/abf-browser/api/v3/folder-info/?folder=` + folderPath;
+        this.setState({ folder: "Loading..." })
+        const folderPath = 'X:/Data/SD/practice/Scott/2022/2022-01-04-AON';
+        const url = `http://192.168.1.9/abf-browser/api/v3/abf-menu-items/?folder=` + folderPath;
         fetch(url)
             .then(response => response.json())
             .then(json => {
@@ -42,30 +64,13 @@ class AbfParentList extends React.Component {
             })
     }
 
-    getParentHtml(parent) {
-        return (
-            <div key={parent.path}>
-                <span style={{ backgroundColor: parent.color, paddingLeft: 3, paddingRight: 3 }}>
-                    {parent.abfID}
-                </span>
-                <span>
-                    ({parent.childAbfs.length})
-                </span>
-                <span style={{ paddingLeft: 5, opacity: .5 }}>
-                    {parent.comment}
-                </span>
-            </div>
-        );
-    }
-
     render() {
         return (
             <div>
                 <h1>{this.state.folder}</h1>
                 <ul>
-                    {this.state.parents.map(parent => this.getParentHtml(parent))}
+                    {this.state.parents?.map(parent => parent.getHtml())}
                 </ul>
-                <button onClick={() => this.fetchParents()}>fetch</button>
             </div>
         )
     }
