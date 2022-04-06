9998c23bbeaeb88d4903aeb96005080fe65ae76d
Scott W Harden <swharden@gmail.com>
2022-04-06 16:06:52 -0400

    dev: fix protocol in URL

--- abfjs/deploy-1.bat
+++ abfjs/deploy-1.bat
@@ -1,3 +1,3 @@
 rmdir "C:\Users\swharden\Documents\GitHub\ABF-browser\abfjs\build" /Q /S
-set PUBLIC_URL=https://192.168.1.9/abf-browser/
+set PUBLIC_URL=http://192.168.1.9/abf-browser/
 npm run build
