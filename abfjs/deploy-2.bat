8515486cb13359da00809cd1986a2f6767573126
Scott W Harden <swharden@gmail.com>
2022-04-06 16:01:52 -0400

    dev: update deployment scripts

---
+++ abfjs/deploy-2.bat
@@ -0,0 +1,3 @@
+rmdir "X:\Lab Documents\network\htdocs\abf-browser" /Q /S
+robocopy "C:\Users\swharden\Documents\GitHub\ABF-browser\abfjs\build" "X:\Lab Documents\network\htdocs\abf-browser" /E /NJH /NFL /NDL /NJS
+pause
