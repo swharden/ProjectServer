cdcf8f6beb8af85999d9da54c928d44c8c56b863
Scott W Harden <swharden@gmail.com>
2022-05-23 12:34:46 -0400

    Bind IP/port based on command argument

--- src/Server/Program.cs
+++ src/Server/Program.cs
@@ -1,41 +1,36 @@
-using Microsoft.AspNetCore.ResponseCompression;
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
 
-namespace Company.WebApplication1
+var app = builder.Build();
+
+if (args is null || args.Length != 1)
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            var builder = WebApplication.CreateBuilder(args);
-
-            // Add services to the container.
-
-            builder.Services.AddControllersWithViews();
-            builder.Services.AddRazorPages();
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseWebAssemblyDebugging();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Error");
-            }
-
-            app.UseBlazorFrameworkFiles();
-            app.UseStaticFiles();
-
-            app.UseRouting();
-
-
-            app.MapRazorPages();
-            app.MapControllers();
-            app.MapFallbackToFile("index.html");
-
-            app.Run();
-        }
-    }
+    Console.WriteLine($"No argument given. Listening on default localhost URLs.");
 }
+else
+{
+    string url = args[0];
+    Console.WriteLine($"Argument provided. Listening on {url}");
+    app.Urls.Add(url);
+}
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error");
+}
+
+app.UseBlazorFrameworkFiles();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapRazorPages();
+app.MapControllers();
+app.MapFallbackToFile("index.html");
+
+app.Run();
