e2c3b85f33d06ba6a4fdddd68a37d76a9132c564
Scott W Harden <swharden@gmail.com>
2022-05-23 12:00:00 -0400

    delete ReactJS project

--- abfjs/deploy-1.bat
+++
@@ -1,3 +0,0 @@
-rmdir "C:\Users\swharden\Documents\GitHub\ABF-browser\abfjs\build" /Q /S
-set PUBLIC_URL=http://192.168.1.9/abf-browser/
-npm run build
--- abfjs/deploy-2.bat
+++
@@ -1,4 +0,0 @@
-rmdir "X:\Lab Documents\network\htdocs\abf-browser" /Q /S
-robocopy "C:\Users\swharden\Documents\GitHub\ABF-browser\abfjs\build" "X:\Lab Documents\network\htdocs\abf-browser" /E /NJH /NFL /NDL /NJS
-robocopy "C:\Users\swharden\Documents\GitHub\ABF-browser\api" "X:\Lab Documents\network\htdocs\abf-browser\api" /E /NJH /NFL /NDL /NJS
-pause
