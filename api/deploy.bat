0d3bb99ad2ff91f738652a3af95123b3c4f1cd47
Scott W Harden <swharden@gmail.com>
2022-05-23 12:00:13 -0400

    delete PHP API

--- api/deploy.bat
+++
@@ -1,6 +0,0 @@
-:: deploy manually via the X-drive because the remote server does 
-:: not have internet access and can't pull the latest code using git
-
-rmdir "X:\Lab Documents\network\htdocs\abf-browser\api" /Q /S
-robocopy "C:\Users\swharden\Documents\GitHub\ABF-browser\api" "X:\Lab Documents\network\htdocs\abf-browser\api" /E /NJH /NFL /NDL /NJS
-::pause
