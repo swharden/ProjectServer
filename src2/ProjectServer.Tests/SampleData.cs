c555826e3f68d74a3fce5523731c480f837720ec
Scott W Harden <swharden@gmail.com>
2022-07-08 11:19:46 -0400

    create test project

---
+++ src2/ProjectServer.Tests/SampleData.cs
@@ -0,0 +1,13 @@
+﻿namespace ProjectServer.Tests;
+
+internal static class SampleData
+{
+    public static string Folder => Path.GetFullPath("../../../../../dev/sample-data");
+
+    [Test]
+    public static void Test_DataFolder_Exists()
+    {
+        Console.WriteLine(Folder);
+        Assert.That(Directory.Exists(Folder));
+    }
+}
