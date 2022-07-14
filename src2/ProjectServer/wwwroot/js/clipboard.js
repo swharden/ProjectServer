b4dbdca12bb1681cf2c3f4d3535784398e102d33
Scott W Harden <swharden@gmail.com>
2022-07-14 11:57:54 -0400

    create clipboard JavaScript

---
+++ src2/ProjectServer/wwwroot/js/clipboard.js
@@ -0,0 +1,12 @@
+﻿function CopyToClipboard(textToCopy) {
+    var input = document.createElement("input");
+    document.body.appendChild(input);
+    input.value = textToCopy;
+    input.select();
+    document.execCommand("copy");
+    document.body.removeChild(input);
+}
+
+function CopySetpath(filePath) {
+    CopyToClipboard(`setpath "${filePath}";`);
+}
