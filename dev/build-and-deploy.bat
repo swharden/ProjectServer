effe31f123c075fa824f368c25e8750d98f39446
Scott W Harden <swharden@gmail.com>
2022-07-20 11:31:58 -0400

    update deployment script

--- dev/build-and-deploy.bat
+++ dev/build-and-deploy.bat
@@ -1,4 +1,4 @@
 rmdir /s /q X:\Software\ProjectServer
-dotnet publish --configuration Release ..\src2
-robocopy ..\src2\ProjectServer\bin\Release\net6.0\publish X:\Software\ProjectServer /E /NJH /NFL /NDL
+dotnet publish --configuration Release ..\src
+robocopy ..\src\ProjectServer\bin\Release\net6.0\publish X:\Software\ProjectServer /E /NJH /NFL /NDL
 pause
