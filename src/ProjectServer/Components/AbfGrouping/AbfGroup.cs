ae9fbb119ca8600d1efe8499c6e0a6954f3a53f3
Scott W Harden <swharden@gmail.com>
2022-07-21 11:43:04 -0400

    use buttons for origin commands

--- src/ProjectServer/Components/AbfGrouping/AbfGroup.cs
+++ src/ProjectServer/Components/AbfGrouping/AbfGroup.cs
@@ -6,14 +6,12 @@ public class AbfGroup
 {
     public string Title { get; set; } = string.Empty;
     public string[] NotesLines { get; set; } = Array.Empty<string>();
-    public string[] OriginLines { get; set; } = Array.Empty<string>();
     public AbfParentInfo[] Parents { get; set; } = Array.Empty<AbfParentInfo>();
 
     public AbfGroup(string title, AbfParentInfo[] parents)
     {
         Title = title;
         Parents = parents;
-        OriginLines = parents.Select(x => $"setpath \"{x.AbfFilePath}\"").ToArray();
 
         // only load notes if all parents are in the same folder
         if (parents.Select(x => x.AbfFolder).Distinct().Count() == 1)
